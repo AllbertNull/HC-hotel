3331bea695ecb9b6388de7f901e63a4564d5e820
Null_忍 <patataOwOsi@gmail.com>
2024-10-04 11:24:21 -0600

    Add files via upload

--- src/app/servicios/cargar-scripts.service.ts
+++ src/app/servicios/cargar-scripts.service.ts
@@ -1,18 +1,26 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 
 @Injectable({
 	providedIn: 'root',
-})
-export class CargarScriptsService {
+  })
+  export class CargarScriptsService {
 
-	constructor() {}
+	constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-	Carga( archivos: string[] ) {
-		for ( let archivo of archivos ) {
+	Carga(archivos: string[]) {
+	  // Verificar si estamos en el entorno del navegador
+	  if (isPlatformBrowser(this.platformId)) {
+		for (let archivo of archivos) {
+		  const scriptSrc = `./assets/js/${archivo}.js`;
+		  if (!document.querySelector(`script[src="${scriptSrc}"]`)) {
 			let script = document.createElement("script");
-			script.src = "./assets/js/" + archivo + ".js";
-			let body = document.getElementsByTagName("body")[0];
-			body.appendChild( script );
+			script.src = scriptSrc;
+			script.onload = () => console.log(`${archivo} cargado correctamente.`);
+			script.onerror = () => console.error(`Error al cargar ${archivo}.`);
+			document.body.appendChild(script);
+		  }
 		}
+	  }
 	}
-}
+  }
