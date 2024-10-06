8d7974161ebdc1e9f4e1358072869d09dfb0d2c5
Null_忍 <patataOwOsi@gmail.com>
2024-10-05 23:37:09 -0600

    Add files via upload

--- src/app/pages/hotel-admin/hotel-admin.component.ts
+++ src/app/pages/hotel-admin/hotel-admin.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RoomPrevComponent } from '../../components/room-prev/room-prev.component';
 import { AddRoomComponent } from '../../components/add-room/add-room.component';
+import { CargarScriptsService } from '../../servicios/cargar-scripts.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-hotel-admin',
 	standalone: true,
-	imports: [RoomPrevComponent, AddRoomComponent],
+	imports: [RoomPrevComponent, AddRoomComponent, RouterModule],
 	templateUrl: './hotel-admin.component.html',
 	styleUrl: './hotel-admin.component.css',
 })
-export class HotelAdminComponent {}
+export class HotelAdminComponent implements OnInit{
+
+	constructor(private _CargarScripts:CargarScriptsService) {
+		_CargarScripts.Carga(["dropdown"]);
+	}
+
+	ngOnInit(): void {
+		throw new Error('Method not implemented.');
+	}
+
+}
