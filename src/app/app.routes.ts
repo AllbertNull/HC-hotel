a05a14d3e53e486cd92bb80d33782d8aa319e1cf
Null_忍 <patataOwOsi@gmail.com>
2024-10-05 23:36:18 -0600

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
+import { HotelAdminComponent } from './pages/hotel-admin/hotel-admin.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AddRoomComponent } from './components/add-room/add-room.component';
 
 export const routes: Routes = [
+	{path: '', component: LandingComponent},
+	{
+		path: 'admin', component: HotelAdminComponent,
+		children: [
+			{path:'cuarto', component: AddRoomComponent},
+		]
+	}
 ];
